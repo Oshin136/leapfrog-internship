e295e276b40c34fa0fea8859abe0072621cce626
Oshin Gansi <os.gansi@gmail.com>
2022-06-15 15:15:50 +0545

    add car link assignment

---
+++ assignments/Assignmwnt-10/src/server.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+
+
+app.listen(3333, () => {
+  console.log("testing");
+});
